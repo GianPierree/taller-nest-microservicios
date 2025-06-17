696daef15c94f20c5d7fc260afaa418bf8101c70
Gian Pierre Munoz Montoya <gian0291@gmail.com>
2025-06-16 21:35:36 -0500

    feat: add gitacctions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola Mundo desde AWS Lambda - Serverless';
+    return 'Hola Mundo desde AWS Lambda - Serverless - GitActions';
   }
 }
